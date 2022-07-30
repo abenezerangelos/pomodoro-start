934c0072e2b20adb2cc8959b69de28aa78d58348
The Author <Ebenezer.Abate@wsu.edu>
2022-07-30 01:28:42 -0700

    This is the ultimate version with added pause_functionality and even proper cycle ending and beginning transitions. You have finished it but we need to still clean the code by bringing the array objects to the beginning of the function instead of the end. We also need to clean out the debug the statements. Still not done yet, needs more cleaning.

--- pause_functionality.py
+++ pause_functionality.py
@@ -1,3 +1,5 @@
+# Not the clean version still looking very nasty as of now.
+
 # from tkinter import *
 # from time import *
 # # Python program killing
@@ -135,11 +137,12 @@
 # root.mainloop()
 #Import tkinter library
 from tkinter import *
+
 #Create an instance of tkinter frame
 # win = Tk()
 # #Set the geometry
 # win.geometry("750x250")
-# #Create a Text Widget
+# #Create a Text WIdget
 # text= Text(win, width= 30, height= 10)
 # text.insert(INSERT, "Hello World!")
 # #Activate the focus
@@ -161,16 +164,16 @@ from threading import Thread
 # TODO 0: Fix the tick label first, DONE
 # TODO 1: DONE, Try and figure out why the recursion doesn't work properly when the window.update()-(use update_idle() which is a lot better) is included inside the work part i.e. why isn't the break part being expressed
 # So to do this part my idea is to set the countdown using after method inside a start countdown method and in this method also starting the timer mechanism with the same button and even destroying and starting the whole UI using after methodand we can basically use the mainloop to continually check if the value of self.reset has changed. And so we will be able to update and therefore reset the whole window including the frame or any other widget.
-# TODO 2: Use bind to get around this problem
-# TODO 3: Use threading.event
+# TODO 2:Hopefully done soon,  Use bind to get around this problem
+# TODO 3:We will see,  Use threading.event
 # TODO 4: DONE, Use "with open as"
 # TODO 5: DONE, Regardless try your best to solve this problem with only Todo 1.
-# TODO Tip: Use a combination of TODO 2,3 and 6 to achieve a very seamless and perfectly functioning program.
-# TODO 6: Try using the App class method and try sending from the main to the thread mainloop
-# TODO 7: Add the pause functionality, when pressing the start button twice and if pressed again it would function like the play button. so if button is pressed i.e. button_pressed%2==0
-# TODO 8: Don't let pressing start once the program has started have any effects, basically make it unfunctional except for when starting the program from the beginning or reset.
-# TODO 9: DONE, Instead of using the after callback instead use the OS time to keep track of the countdown and also manage the transition between Work and Break because you don't want your time to be fast or slow depending on other processes, if we want a program that is efficient we want to tie it to the OS Kernel time.
-# TODO 10: ADD SOUND, for notification puposes! if possible let it have its own administrative access for sound where it uses maximum volume to notify. This part might be a little too advanced but this is the beauty of programming, the hill, feel me!
+# TODO Tip: Will be done soon, possibly, hopefully, Use a combination of TODO 2,3 and 6 to achieve a very seamless and perfectly functioning program.
+# TODO 6: This doesn't really work, sorry, Try using the App class method and try sending from the main to the thread mainloop
+# TODO 7: Thank God, finally successfully done,  Add the pause functionality, when pressing the start button twice and if pressed again it would function like the play button. so if button is pressed i.e. button_pressed%2==0
+# TODO 8: Easily DONE, Don't let pressing start once the program has started have any effects, basically make it unfunctional except for when starting the program from the beginning or reset.
+# TODO 9: DONE, kinda, i am still using the after callback but it is also depepnding on the time() method. One thing I want to try is the event and bind method without no help from the after callback.  Instead of using the after callback instead use the OS time to keep track of the countdown and also manage the transition between Work and Break because you don't want your time to be fast or slow depending on other processes, if we want a program that is efficient we want to tie it to the OS Kernel time.
+# TODO 10:DONE, ADD SOUND, for notification puposes! if possible let it have its own administrative access for sound where it uses maximum volume to notify. This part might be a little too advanced but this is the beauty of programming, the hill, feel me!
 # Improvement for "todo 9, kinda done, not the sound part, if you can't another very specific os timer just use time.time() function and make it work and also make your application use sound
 # TODO 11:(Will do almost already done, ez, I don't see the applicability- actually instead ask the user if they want to stop or start another round of the timer) ADD STOP/END feature when the app ends for a small amount of time, and on top of that try removing the last break and replace it with the end/stop feature.
 # TODO 12:(Tried my best, except for the fact that I haven't tried any of the threading, event or binding strategies just yet) Make sure this program uses as minimum of resources as possible, make sure it doesn't heavily interfere with other apps and your os as much as possible. Either find a data sturcture that is more efficient than a list or update the strategy you are using to count. Maybe threading or binding might fix it all.
@@ -210,44 +213,45 @@ def button_click():
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
-from winsound import *
+
 # ichecker = False # should be deprecated, not necessarily useful given the fact that you can just use the time to check if it was the start and then activate based on that.
 def timer_mechanism(identifier,value,dif):
     minute=0
-
+    print(i)
     value=int(value)
     counter=int(value-dif)
     print(dif,"CLOCK DIFFERENCE BETWEEN START AND END")
-    global rest_pause
-    global only_once
-    print(f"\n3.This is the one thing that shouldn't be changing before hand: {rest_pause} \n")
+    print(value)
+    print(counter)
+
     if identifier=="Work" and int(dif)==0 :
 
         timer.config(text="Work", fg=GREEN)
+        if i[0]!=4:
+            timer.bell(window)
+            print("\n\TIMER\n\n time")
+        print("Time has been displayed")
         window.deiconify()
         window.attributes("-topmost", 1)
         window.attributes("-topmost", 0)
 
-    elif (identifier=="Break" or identifier=="Long Break") and only_once==True:
-        if identifier=="Break":
-            timer.config(text="Break", fg=PINK)
+    elif (identifier=="Break" or identifier=="Long Break")and int(dif)==0:
+        print(dif, "IT AIN'T MFING ",0, "SO SHUSH PLEASE!!!")
+        timer.config(text="Break", fg=PINK)
 
-        if identifier=="Long Break":
-            timer.config(text="Break", fg=RED)
         if i[0]==3:
             global frame
             frame = Frame(window, width=20, height=20)
             frame.grid(row=3, column=1)
-            keeper=[Frame]
+
         tick = Label(frame, text="✔", font=(FONT_NAME, 12, "bold"), bg=YELLOW, fg=GREEN)
-        MessageBeep(MB_ICONEXCLAMATION)
+        tick.bell(window)
         tick.pack(side=LEFT)
         window.deiconify()
         window.attributes("-topmost", 1)
         window.attributes("-topmost", 0)
-        only_once=False
         # ichecker=False
-    print(f"\n2.{identifier}, {dif}, {counter}.\nThis helps debugging with the long break and the destruction of the old window and start of new one\n")
+
     if counter<60 :
       if counter/1<10:
         canvas.itemconfig(clock_counter, text=(f"00:0{int(counter/1)}"))
@@ -259,6 +263,7 @@ def timer_mechanism(identifier,value,dif):
 
     elif counter>=60 :
 
+            print("Time has been displayed")
             transition=int(counter/1)
             while transition>=60:
                 minute+=1
@@ -385,43 +390,45 @@ def timer_mechanism(identifier,value,dif):
 #        beginning()
 # -----------------------------New countdown mechanism----------------------------#
 i = [4]
+count_identifier=0
+
 
 
 def start_countdown():
     with open("button_click") as reset:
         timer_reset=reset.read()
+
     global count_identifier
+    print(count_identifier,11111111111)
 
-    global rest_pause
-    print(rest_pause, "!!!!", count_identifier, "$$$$$$")
-    if count_identifier==0 or (rest_pause==False and timer["text"]=="Break") :
-
-        start_work[0]=time()
-    else:
-        global counter_dif
-        try:
-            start_work[0]=time()-counter_dif-1
-        except:
-            start_work[0]=time()
-
+    global counter_dif
+    if count_identifier>0 and count_identifier%2==0:
+        start_work[0]=time()-counter_dif-1
+    elif count_identifier==0:
+        start_work[0] = time()
+    print(start_work[0],"Not that important")
 
     if count_identifier%2==1:
         global after_object
-
-
         window.after_cancel(after_object)
-    elif timer_reset!="Clicked" and i[0] >= 0 and count_identifier%2==0:
+    print(start_work[0],"SHOULD BE THE SAME")
+    global pause_identifiier
+    if timer_reset!="Clicked" and i[0] >= 0 and count_identifier%2==0:
+        print(start_work[0],"IMPORTANT<IMPORTANT>")
 
         # keep listening to the function that receives from the button while doing an after for the 25 min and 10 min respectively.3
 
 
 
         # the solution to this problem is to break up the after process instead of doing it all at the same time make use of small increments so that this after could finish withing a second or two and then go to another function that contains all the timer_reset.click values update and come everytime until we reach a limit somewhere probably an array value that will be updated until we get to the 25 minute mark so this insures as the signal from the button will be accepted.
+        print("POOOPIE BUTTHOLE BABY")
 
-        if rest_pause==True:
-            rest()
-        else:
+        if pause_identifier=="Work":
             execute("Work")
+        elif pause_identifier=="Break":
+            execute("Break")
+        elif pause_identifier=="Long Break":
+            execute("Long Break")
 
         # start=time()
         # end=time()
@@ -429,7 +436,7 @@ def start_countdown():
         #     end=time()
         #     timer_reset.click=timer_reset.timer_command()
 
-    else:
+    elif timer_reset=="Clicked" or i[0]<0:
         window.destroy()
         reset_allvalues()
         beginning()
@@ -437,95 +444,50 @@ def start_countdown():
 def reset_allvalues():
 
     i[0] = 4
+    global count_identifier
+    count_identifier=0
     with open("button_click",mode="w") as reset:
          reset.write("Not Clicked")
     # counter[0]=WORK_MIN
-
-def rest():
-    global rest_pause
-    with open("button_click") as reset:
-        timer_reset=reset.read()
-
-    global count_identifier
-    if count_identifier==1 or timer["text"]=="Work":
-        print("\n1.",timer["text"],"\n")
-        start_work[0]=time()
-    else:
-        global counter_dif
-        start_work[0]=time()-counter_dif-1
-
-    if timer_reset=="Not Clicked" and i[0]>=0:
-        #logic is a bit complicated not that simple, it is not easy to read but idk y i really like it but it shouldn't be this way, code usually shouldn't have a lot of global variables and logic shouldn't be tangled up
-        #one thing that is costing us readability is using the adder variables at the end of the function actually bring it up it will be way easier that way giving us a clear distinction between functions.
-        if i[0] >1 or i[0]==1 and rest_pause==True:
-            print(f"\n4.What is its value before execution {rest_pause}")
-            execute("Break")
-            print(f"5. What is its value after: {rest_pause}\n")
-        # i feel like we can make this code look a lot cleaner than it is supposed to be, mainly by adding the self-referencing variables to the beginnning of the function
-
-        elif (i[0] ==0 and rest_pause==True) or (i[0]==1 and rest_pause==False):
-            execute("Long Break")
-
-
-
-        else:
-            print("DESTRUCTION")
-            window.destroy()
-            reset_allvalues()
-            beginning()
-
-    else:
-        print("DESTRUCTION!!!!!")
-        window.destroy()
-        reset_allvalues()
-        beginning()
-    if rest_pause!=True:
-        i[0] -= 1
-        global only_once
-        only_once=True
-
-    rest_pause=True
-
-
 # counter=[WORK_MIN]
 #create one function that keeps calling time and opening the button_click file every once in a while checking to see if the time.time() inside the function minus the initial outside the function equal to the amount and for the timer mechanism just substract the difference from the initial WORK_MIN or BREAK_MIN.
 #given that we can actually use the with open as function right now as is with the "button_click" array. The with open as function is just substituting our Reset() class and nothing else.
 
 def execute(identifier):
+
     global after_object
-    global rest_pause
     if identifier=="Work":
-        after_object=window.after(500,mirror,WORK_MIN,identifier)
+        after_object=window.after(1000,mirror,WORK_MIN,identifier)
 
     if identifier=="Break":
-        print(f"\n6.This is before the after_object executes: {rest_pause}\n")
-        after_object=window.after(500,mirror,SHORT_BREAK_MIN,identifier)
-        print(f"\n7. This is right after the after_object: {rest_pause}\n")
+        print("working at some point")
+        print(i)
+        print(start_work)
+        after_object=window.after(1000,mirror,SHORT_BREAK_MIN,identifier)
     if identifier=="Long Break":
-        after_object=window.after(500,mirror,LONG_BREAK_MIN,identifier)
-    print(after_object, 333333333333333333333333333333333333333333333333333333333333333333333333333333)
+        after_object=window.after(1000,mirror,LONG_BREAK_MIN,identifier)
 
 def mirror(value,identifier):
-
+    global pause_identifier
+    print("WHATEVER, JUST PLEASE WORK!")
     value=int(value)
-    global rest_pause
     with open("button_click") as reset:
         timer_reset=reset.read()
-
+    print(timer_reset,"DEBUG2!!!")
     end=time()
     global counter_dif
     counter_dif=int(end-start_work[0]-1)
-
+    print(int(counter_dif),"EVERY STEP OF THE WAY!")
     if timer_reset=="Clicked":
         window.destroy()
         reset_allvalues()
         beginning()
     elif counter_dif<value:#elif counter[0]>=1:
-        print(f"\n8. Debugger {rest_pause}, this should be absolutely false")
+        print("this is working too")
         timer_mechanism(identifier=identifier,value=value,dif=counter_dif)
-        print(f"9. Debugger {rest_pause}, this should be absolutely false, so shall does this.\n")
         # print(button_click)
         if identifier=="Break":
+            print("this is working three")
             execute("Break")
         if identifier=="Long Break":
             execute("Long Break")
@@ -539,30 +501,46 @@ def mirror(value,identifier):
 
         timer_mechanism(identifier=identifier, value=value, dif=counter_dif)
         if identifier=="Work":
-
             # if i[0]==1:
             #     counter[0]=LONG_BREAK_MIN
             # else:
             #     counter[0] = SHORT_BREAK_MIN
-            rest_pause=False
-            global count_identifier
-            count_identifier=1
+            print("\n\nWHAT ABOUT THIS, THIS HAS TO START EXECUTING AGHHHH\n\n")
+            if timer_reset == "Not Clicked":
+                print(i[0])
+                if i[0] > 1:
+                    print("\n\n EXECUTION!!!!!!!!!!!!\n\n")
+                    pause_identifier="Break"
+                    start_work[0]=time()
+                    execute("Break")
+                elif i[0] == 1:
+                    start_work[0]=time()
+                    pause_identifier="Long Break"
+                    execute("Long Break")
 
 
 
-            counter_dif=0
-            rest()
+                else:
+                    print("DESTRUCTION")
+                    window.destroy()
+                    reset_allvalues()
+                    beginning()
+
+            else:
+                print("DESTRUCTION!!!!!")
+                window.destroy()
+                reset_allvalues()
+                beginning()
+            i[0] -= 1
         if identifier=="Break" :
             # counter[0] = WORK_MIN
-
-            count_identifier=0
-
-            counter_dif = 0
-            rest_pause=False
-            start_countdown()
-        if identifier=="Long Break":
-            sleep(0.5)
-            Beep(1000,1500)
+            global count_identifier
+            count_identifier=1
+            start_work[0]=time()
+            pause_identifier="Work"
+            execute("Work")
+        elif identifier=="Long Break":
+            window.bell()
             window.destroy()
             reset_allvalues()
             beginning()
@@ -575,29 +553,34 @@ def beginning():
     window = Tk(className="Pomodoro")
     window.title("Pomodoro")
     window.config(pady=50, padx=100, bg=YELLOW)
-
+    print("W")
 
     global timer
     timer = Label(text="Timer", font=(FONT_NAME, 50, "bold"), bg=YELLOW, foreground=GREEN)
     timer.grid(row=0, column=1)
+    print("H")
     global canvas
     canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
     tomato = PhotoImage(file="tomato.png")
     canvas.create_image(100, 100, image=tomato)
     global clock_counter
+    print("Y")
     clock_counter=canvas.create_text(100, 120, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
     canvas.grid(row=1, column=1)
-    global rest_pause
-    rest_pause=False
-    global count_identifier
-    count_identifier=0
+    global counter_dif
+    counter_dif=0
+    global pause_identifier
+    pause_identifier="Work"
+    print("I")
     start = Button(text="Start", highlightthickness=0, borderwidth=0, command=start_countdown)
     start.grid(row=2, column=0)
     end = Button(text="Reset", highlightthickness=0, borderwidth=0, command=button_click)
     end.grid(row=2, column=2)
+    print("S")
     window.deiconify()
-    window.attributes("-topmost", 1)
 
+    window.attributes("-topmost", 1)
+    print("it taking so long")
     window.mainloop()
 
 
